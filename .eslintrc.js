de3a24fd848ba077a9426be9e75de0cf12cadad8
Cody Olsen <stipsan@gmail.com>
2022-05-29 03:48:57 +0200

    chore: update deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,11 @@
 module.exports = {
   root: true,
   extends: ['react-app'],
-  plugins: ['testing-library'],
+  plugins: ['testing-library', 'simple-import-sort'],
+  rules: {
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+  },
   overrides: [
     // Only uses Testing Library lint rules in test files
     {
